2c8effa188d3f350d7b274bd6bf2db424514b6ad
Aymeric Galan <aymeric.galan@gmail.com>
2023-01-10 14:10:37 +0100

    Fix issue with Wavelet tests that did not work anymore with 64bit floats

--- test/wavelet_test.py
+++ test/wavelet_test.py
@@ -5,21 +5,21 @@ import pytest
 import os
 import numpy as np
 import numpy.testing as npt
-# from scipy import signal, ndimage
-# import pysap
+
+from jax.config import config
+config.update("jax_enable_x64", True)
 
 import utax
 from utax.wavelet import *
 
 
-
 class TestWaveletTransform(object):
 
-
     def setup(self):
-        # load some test images
         utax_path = os.path.dirname(utax.__path__[0])
         data_path = os.path.join(utax_path, 'test', 'data')
+
+        # load some test images obtained using from pysparse (sparse2d) from PySAP
         self.image = np.load(os.path.join(data_path, 'galaxy_image.npy'))
         self.coeffs_st_gen1 = np.load(os.path.join(data_path, 'galaxy_starlet_coeffs_gen1_pysparse.npy'))
         self.coeffs_st_gen2 = np.load(os.path.join(data_path, 'galaxy_starlet_coeffs_gen2_pysparse.npy'))
@@ -31,7 +31,7 @@ class TestWaveletTransform(object):
         coeffs = starlet.decompose(self.image)
         npt.assert_almost_equal(coeffs, self.coeffs_st_gen1, decimal=6)
 
-        # 1st generation starlet
+        # 2nd generation starlet
         starlet = WaveletTransform(self.n_scales, wavelet_type='starlet', second_gen=True)
         coeffs = starlet.decompose(self.image)
         npt.assert_almost_equal(coeffs, self.coeffs_st_gen2, decimal=6)
@@ -42,7 +42,14 @@ class TestWaveletTransform(object):
         image = starlet.reconstruct(self.coeffs_st_gen1)
         npt.assert_almost_equal(image, self.image, decimal=6)
 
-        # 1st generation starlet
+        # 2nd generation starlet
         starlet = WaveletTransform(self.n_scales, wavelet_type='starlet', second_gen=True)
         image = starlet.reconstruct(self.coeffs_st_gen2)
         npt.assert_almost_equal(image, self.image, decimal=6)
+
+    def test_scale_norms(self):
+        starlet = WaveletTransform(self.n_scales)
+        norms = starlet.scale_norms
+        assert norms.size == self.n_scales + 1
+        # check that values are decreasing (a Dirac impulse has more power in high frequencies)
+        assert np.all(norms[:-1] > norms[1:])

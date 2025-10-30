eb8db85ca50db230426001e6fd9b0804e6b81f47
Aymeric Galan <aymeric.galan@gmail.com>
2025-10-30 17:15:53 +0100

    Update version number and python+jax requirements

--- utax/info.py
+++ utax/info.py
@@ -5,7 +5,7 @@ This module provides some basic information about the package.
 """
 
 # Set the package release version
-version_info = (0, 0, 1)
+version_info = (0, 0, 2)
 __version__ = '.'.join(str(c) for c in version_info)
 
 # Set the package details
@@ -14,10 +14,10 @@ __email__ = 'aymeric.galan@gmail.com'
 __year__ = '2022'
 __url__ = 'https://github.com/aymgal/utax'
 __description__ = 'Utility functions for signal processing, compatible with the differentable programming library JAX.'
-__python__ = '>=3.7'
+__python__ = '>=3.10'
 __requires__ = [
-    'jax>=0.3.14', 
-    'jaxlib>=0.3.14', 
+    'jax>=0.5.0', 
+    'jaxlib>=0.5.0', 
 ]  # Package dependencies
 
 # Default package properties
